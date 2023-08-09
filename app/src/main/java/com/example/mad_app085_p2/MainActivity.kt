77a6307f8b715f41feb82baafa15156721eaf8b3
Dhruv Patel <dhruvpatel20@gnu.ac.in>
2023-08-09 20:02:34 +0530

    f

--- app/src/main/java/com/example/mad_app085_p2/MainActivity.kt
+++ app/src/main/java/com/example/mad_app085_p2/MainActivity.kt
@@ -1,16 +1,14 @@
 package com.example.mad_app085_p2
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
-import kotlin.math.log
-
+import com.google.android.material.snackbar.Snackbar
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
     override fun onStart() {
         super.onStart()
         displayMsg("Onstart Method called")
@@ -31,9 +29,9 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
         displayMsg("onDestroy Method called")
     }
-
     fun displayMsg(msg:String){
-        Toast.makeText(this, "$msg", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+        Snackbar.make(findViewById(R.id.mycl), msg,Snackbar.LENGTH_SHORT).show()
+        Log.i("MainActivity", msg)
     }
-
 }
